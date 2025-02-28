8a87053fbac14244e165b75c5c05a5d966a6aed9
Edwar <echamale018@gmail.com>
2025-02-28 16:41:34 -0600

    Modificacion en la conexión para usar sequelize

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,22 @@
-import mysql from 'mysql2/promise';
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
-dotenv.config
+dotenv.config();
+
+export const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql'
+    }
+);
 
 export const dbConnection = async () => {
     try {
-        const connection = await mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAMW,
-        });
-
-        console.log('Conectado a la base de datos.')
-        return connection;
+        await sequelize.authenticate();
+        console.log('Conectado a la base de datos')
     } catch (error) {
         console.error('Error al conectarse a la base de datos.', error)
         process.exit(1)
