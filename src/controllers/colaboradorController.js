f08929569301449a7b924a36730cb251f089b370
Edwar <echamale018@gmail.com>
2025-03-01 13:03:12 -0600

    Endpoint de actualizar colaboradores

--- src/controllers/colaboradorController.js
+++ src/controllers/colaboradorController.js
@@ -3,8 +3,9 @@ import { Colaborador } from "../models/colaboradorModel.js";
 export const createColaborador = async (req, res) => {
     try {
         const { nombre, apellido, direccion, edad, profesion, estadoCivil } = req.body;
-        console.log(edad, "nombe")
+
         const colaborador = await Colaborador.create({ nombre, apellido, direccion, edad, profesion, estadoCivil});
+
         return res.status(201).json({ message: 'Colaborador creado', colaborador});
     } catch (error) {
         console.log("error", error)
@@ -18,5 +19,23 @@ export const getColaboradores = async (req, res) => {
         return res.status(200).json(colaboradores);
     } catch (error) {
         return res.status(500).json({ message: 'Error al obtener los colaboradores' })
+    }
+}
+
+export const updateColaborador = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { nombre, apellido, direccion, edad, profesion, estadoCivil } = req.body;
+
+        const colaborador = await Colaborador.findByPk(id);
+
+        if(!colaborador) {
+            return res.status(404).json({ message: 'Colaborador no encontrado' });
+        }
+
+        await colaborador.update({ nombre, apellido, direccion, edad, profesion, estadoCivil });
+        return res.status(200).json({ message: 'Colaborador actualizado', colaborador});
+    } catch (error) {
+        return res.status(400).json({ message: 'Error al actualizar el colaborador' })
     }
 }
--- src/routes/colaborador.routes.js
+++ src/routes/colaborador.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { createColaborador,
-        getColaboradores
+        getColaboradores,
+        updateColaborador
 } from '../controllers/colaboradorController.js';
 
 const router = express.Router();
 
 router.post('/colaborador', createColaborador);
 router.get('/colaborador', getColaboradores)
+router.put('/colaborador/:id', updateColaborador)
 
 export default router;
